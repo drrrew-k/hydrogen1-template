1024d0b74a716a565cf0ae7aa07d741574cfcbe1
Andrew K <kirilkov.andrew@gmail.com>
2024-10-04 01:02:36 +0300

    Force rebuild

--- app/root.tsx
+++ app/root.tsx
@@ -100,7 +100,7 @@ export async function loader({context}: LoaderArgs) {
 
   const styles = storefront.query(CUSTOM_CSS_QUERY, {
     cache: storefront.CacheLong(),
-  });
+  }); 
   
   //http://localhost:1337/api/store1-settings/1/?populate=*
   const STRAPI_URL = context.env.CMS_API_URL.replace(/\/$/, "");
