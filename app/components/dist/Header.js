a9d246c837c4efbf2e534cfcc98bf6207caa509a
Andrew K <kirilkov.andrew@gmail.com>
2025-08-15 15:53:28 +0300

    Replaced header icons with Heroicons

--- app/components/dist/Header.js
+++ app/components/dist/Header.js
@@ -37,8 +37,8 @@ function HeaderMenu(_a) {
 exports.HeaderMenu = HeaderMenu;
 function HeaderCtas(_a) {
     var isLoggedIn = _a.isLoggedIn, cart = _a.cart;
-    var account_icon_logged = React.createElement("svg", { className: "svg-icon", style: { width: '30px', height: '30px', verticalAlign: 'middle', fill: 'currentColor', overflow: 'hidden' }, viewBox: "0 0 1024 1024", version: "1.1", xmlns: "http://www.w3.org/2000/svg" },
-        React.createElement("path", { d: "M723.43 508.6c-54.123 47.75-125.977 77.056-205.163 77.056-80.364 0-153.4-30.259-207.765-79.421C184.05 539.325 105.81 652.308 105.81 787.277v68.782c0 160.968 812.39 160.968 812.39 0v-68.782c-0.005-131.415-74.22-242.509-194.77-278.677z m-205.163 28.13c140.165 0 254.095-109.44 254.095-244.64S658.668 47.218 518.267 47.218c-139.93 0-253.855 109.675-253.855 244.874 0 135.204 113.925 244.639 253.855 244.639z m0 0" }));
+    var account_icon_logged = React.createElement("svg", { xmlns: "http://www.w3.org/2000/svg", style: { height: "30px" }, fill: "none", viewBox: "0 0 24 24", "stroke-width": "1.5", stroke: "currentColor", "class": "size-6" },
+        React.createElement("path", { "stroke-linecap": "round", "stroke-linejoin": "round", d: "M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" }));
     var account_icon_login = account_icon_logged;
     return (React.createElement("nav", { className: "header-ctas", role: "navigation" },
         React.createElement(HeaderMenuMobileToggle, null),
@@ -56,22 +56,8 @@ function HeaderMenuMobileToggle() {
 function SearchToggle() {
     var open = Aside_1.useAside().open;
     return (React.createElement("button", { className: "reset search-toggle", onClick: function () { return open('search'); } },
-        React.createElement("svg", { style: { height: '30px', width: '30px' }, xmlns: true }),
-        "dc=\"http://purl.org/dc/elements/1.1/\" xmlns:cc=\"http://creativecommons.org/ns#\" xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\" xmlns:svg=\"http://www.w3.org/2000/svg\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:sodipodi=\"http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd\" xmlns:inkscape=\"http://www.inkscape.org/namespaces/inkscape\" width=\"500\" height=\"500\" viewBox=\"0 0 500.00001 500.00001\" id=\"svg4162\" version=\"1.1\" inkscape:version=\"0.92.3 (2405546, 2018-03-11)\" sodipodi:docname=\"Search_Icon.svg\">",
-        React.createElement("defs", { id: "defs4164" }),
-        React.createElement("g", { inkscape: true }),
-        "label=\"Layer 1\" inkscape:groupmode=\"layer\" id=\"layer1\" transform=\"translate(0,-552.36216)\">",
-        React.createElement("g", { id: "g1400", transform: "translate(-4.3609793,-7.6704785)" },
-            React.createElement("path", { inkscape: true }),
-            "connector-curvature=\"0\" id=\"path4714\" d=\"M 232.83952,614.96702 A 154.04816,154.04794 0 0 0 78.79153,769.01382 154.04816,154.04794 0 0 0 232.83952,923.06184 154.04816,154.04794 0 0 0 386.88751,769.01382 154.04816,154.04794 0 0 0 232.83952,614.96702 Z m 0,26.77613 A 129.95832,127.2707 0 0 1 362.79832,769.01382 129.95832,127.2707 0 0 1 232.83952,896.28449 129.95832,127.2707 0 0 1 102.88194,769.01382 129.95832,127.2707 0 0 1 232.83952,641.74315 Z\" style=",
-            { opacity: 1, fill: '#2b0000', fillOpacity: 1, stroke: 'none', strokeOpacity: 1 },
-            " />",
-            React.createElement("rect", { ry: "18.08342", rx: "33.249443", transform: "matrix(0.65316768,0.7572133,-0.60689051,0.79478545,0,0)", y: "319.55432", x: "794.8775", height: "36.16684", width: "173.02675", id: "rect4721", style: { opacity: 1, fill: '#2b0000', fillOpacity: 1, stroke: 'none', strokeOpacity: 1 } }))));
-    svg >
-    ;
-    button >
-    ;
-    ;
+        React.createElement("svg", { xmlns: "http://www.w3.org/2000/svg", style: { height: '30px', width: '30px' }, fill: "none", viewBox: "0 0 24 24", "stroke-width": "1.5", stroke: "currentColor", "class": "size-6" },
+            React.createElement("path", { "stroke-linecap": "round", "stroke-linejoin": "round", d: "m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" }))));
 }
 function CartBadge(_a) {
     var count = _a.count;
@@ -87,9 +73,8 @@ function CartBadge(_a) {
                 url: window.location.href || ''
             });
         } },
-        React.createElement("svg", { xmlns: "http://www.w3.org/2000/svg", width: "30", height: "30", viewBox: "0 0 48 48" },
-            React.createElement("path", { d: "M14 36c-2.21 0-3.98 1.79-3.98 4s1.77 4 3.98 4 4-1.79 4-4-1.79-4-4-4zm-12-32v4h4l7.19 15.17-2.7 4.9c-.31.58-.49 1.23-.49 1.93 0 2.21 1.79 4 4 4h24v-4h-23.15c-.28 0-.5-.22-.5-.5 0-.09.02-.17.06-.24l1.79-3.26h14.9c1.5 0 2.81-.83 3.5-2.06l7.15-12.98c.16-.28.25-.61.25-.96 0-1.11-.9-2-2-2h-29.57l-1.9-4h-6.53zm32 32c-2.21 0-3.98 1.79-3.98 4s1.77 4 3.98 4 4-1.79 4-4-1.79-4-4-4z" }),
-            React.createElement("path", { d: "M0 0h48v48h-48z", fill: "none" })),
+        React.createElement("svg", { xmlns: "http://www.w3.org/2000/svg", width: "30", height: "30", fill: "none", viewBox: "0 0 24 24", "stroke-width": "1.5", stroke: "currentColor", "class": "size-6" },
+            React.createElement("path", { "stroke-linecap": "round", "stroke-linejoin": "round", d: "M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" })),
         React.createElement("span", null,
             " ",
             count === null ? React.createElement("span", null, "\u00A0") : count,
